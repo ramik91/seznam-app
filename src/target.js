dec206b5382147b385258ce264c5e57d2009af84
Jakub Stebel <k.stebel@seznam.cz>
2019-06-20 19:30:33 +0200

    Implement Target
    Add Target class logic
    Create unit tests for Target

---
+++ src/target.js
@@ -0,0 +1,44 @@
+/**
+ * Represents shooting target
+ */
+export class Target {
+    /**
+     * @param {number[]} center 
+     * @param {number} radius 
+     */
+    constructor(center, radius) {
+        /** 
+         * @public
+         * @readonly
+         * @type {number[]}
+         */
+        this.center = center;
+        /**
+         * @public
+         * @readonly 
+         * @type {number} 
+         */
+        this.radius = Math.abs(radius);
+    }
+
+    /**
+     * Try to hit specified point on taget.
+     * Returns true if target is hit or false in case of miss.
+     * @param {number} point 
+     * @example
+     * // Target hit
+     * const target = new Target([0, 0], 5);
+     * const hitResult = taget.tryHit([1, -1]);
+     * @returns {boolean}
+     */
+    tryHit(point) {
+        /** @type {number} */
+        const xDistance = Math.pow(point[0] - this.center[0], 2);
+        /** @type {number} */
+        const yDistance = Math.pow(point[1] - this.center[1], 2);
+        /** @type {number} */
+        const centerDistance = Math.sqrt(xDistance + yDistance);
+
+        return centerDistance <= this.radius;
+    }
+}
